3c4194b8fd80057c7efa991a35bfb81a489d49b6
pedroberrueco <pedroberrueco@hotmail.com>
2023-06-16 10:51:47 +0200

    primer commit

---
+++ src/Ejercicio2/ordinaria_ENDETest.java
@@ -0,0 +1,16 @@
+package Ejercicio2;
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ordinaria_ENDETest {
+
+	@Test
+	public void testEsPalindromo() {
+		assertTrue("reconocer", true);
+		assertTrue("¡arriba la birra!", false);
+		assertTrue("Isaac no ronca así", false);
+	}
+
+}
